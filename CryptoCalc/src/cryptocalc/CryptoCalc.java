cce859262b6f232c35419b827471190c06ed612d
kvsjxd <karanvir.singh61@yahoo.in>
2016-08-19 12:09:46 +0530

    License added to every src file

--- CryptoCalc/src/cryptocalc/CryptoCalc.java
+++ CryptoCalc/src/cryptocalc/CryptoCalc.java
@@ -1,3 +1,23 @@
+/*****************************************************************************
+ * src/CryptoCalc.java: Main application JFrame for CryptoCalc
+ *****************************************************************************
+ * Copyright (C) 2016 Karanvir Singh
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *****************************************************************************/
+
 package cryptocalc;
 
 import javax.swing.JFrame;
@@ -69,10 +89,10 @@ public class CryptoCalc extends JFrame {
 			}
 		});
 		mnHelp.add(mntmAbout);
-		
+
 		JMenu mnLegalInformation = new JMenu("Legal information");
 		mnHelp.add(mnLegalInformation);
-		
+
 		JMenuItem mntmCryptocalcLicense = new JMenuItem("CryptoCalc License");
 		mntmCryptocalcLicense.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -81,7 +101,7 @@ public class CryptoCalc extends JFrame {
 			}
 		});
 		mnLegalInformation.add(mntmCryptocalcLicense);
-		
+
 		JMenuItem mntmOpenSourceLicenses = new JMenuItem("Open source licenses");
 		mntmOpenSourceLicenses.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
---
+++ README.md
@@ -0,0 +1,7 @@
+#CryptoCalc
+
+CryptoCalc is a java application which can generate cryptographic sums of a file.
+
+This source is released as a part of Droid PC Suite, full source and releases of Droid PC Suite can be accessed here, https://github.com/kvsjxd/Droid-PC-Suite
+
+Note: Please use latest java to use this application or this application may not work as expected!
